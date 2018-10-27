25f222d9404bcf20cd658228c5a95257f2809f78
sdakf <sdakftraining@gmail.com>
2018-10-27 14:57:58 +0200

    category parsing done

--- src/test/java/pl/sda/intermediate12/CategoryParseTest.java
+++ src/test/java/pl/sda/intermediate12/CategoryParseTest.java
@@ -1,27 +1,35 @@
 package pl.sda.intermediate12;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class CategoryParseTest {
     @Test
-    public void checkParseOK(){
-        Category novel1 = Category.builder()
-                .depth(1)
-                .id(2)
-                .parentId(1)
-                .name("Powieści")
-                .build();
-        Category novel2 = Category.builder()
-                .depth(1)
-                .id(8)
-                .parentId(7)
-                .name("Powieści")
-                .build();
-        Category classTwo = Category.builder()
-                .depth(2)
-                .id(6)
-                .parentId(4)
-                .name("Klasa druga")
-                .build();
+    public void checkParseOK() {
+        Category novel1 = Category.builder().depth(1).id(2).parentId(1).name("Powieści").build();
+        Category novel2 = Category.builder().depth(1).id(8).parentId(7).name("Powieści").build();
+        Category classTwo = Category.builder().depth(2).id(6).parentId(4).name("Klasa druga").build();
+
+        List<Category> categoryList = InMemoryCategoryDAO.getInstance().getCategoryList();
+
+        Category novel1FromDAO = categoryList.stream()
+                .filter(o -> o.getId().equals(2))
+                .findFirst()
+                .orElse(null);
+        Category novel2FromDAO = categoryList.stream()
+                .filter(o -> o.getId().equals(8))
+                .findFirst()
+                .orElse(null);
+        Category classTwoFromDAO = categoryList.stream()
+                .filter(o -> o.getId().equals(6))
+                .findFirst()
+                .orElse(null);
+
+        Assertions.assertEquals(novel1.getParentId(), novel1FromDAO.getParentId());
+        Assertions.assertEquals(novel2.getParentId(), novel2FromDAO.getParentId());
+        Assertions.assertEquals(classTwo.getParentId(), classTwoFromDAO.getParentId());
+
     }
 }
