7705c00fe5190ec6857a31564260271bdd709d02
sdakf <sdakftraining@gmail.com>
2018-10-28 15:29:38 +0100

    LambdasFinished

--- src/test/java/pl/sda/intermediate12/StreamsAndLambdasTest.java
+++ src/test/java/pl/sda/intermediate12/StreamsAndLambdasTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamsAndLambdasTest {
@@ -27,42 +28,55 @@ public class StreamsAndLambdasTest {
     public void lambdas1() {
         int counter = 0;
         Comparator<String> comparator = (v, b) -> String.valueOf(b.charAt(1)).compareTo(String.valueOf(v.charAt(1)));
-         List<String> list = animals.stream().filter(a -> a != null&&!a.trim().isEmpty()).map(a -> a.trim()).sorted(comparator).collect(Collectors.toList());
+        List<String> list = animals.stream().filter(a -> a != null && !a.trim().isEmpty()).map(a -> a.trim()).sorted(comparator).collect(Collectors.toList());
         for (String animal : list) {
             counter++;
             if (animal == null || animal.trim().isEmpty()) {
                 continue;
             }
-            System.out.print(animal.trim()+(counter<list.size()?", ":"."));
+            System.out.print(animal.trim() + (counter < list.size() ? ", " : "."));
         }
         System.out.println();
         System.out.println(animals.stream()
                 .filter(animal -> animal != null && !animal.trim().isEmpty())
                 .map(animal -> animal.trim())
                 .sorted(Comparator.reverseOrder())
-                .collect(Collectors.joining(", ","",".")));
+                .collect(Collectors.joining(", ", "", ".")));
 
         System.out.println(people.stream()
                 .filter(p -> p.getName().startsWith("A"))
                 .map(e -> e.getName().toUpperCase())
                 .distinct()
-                .sorted((a,b) -> String.valueOf(a.charAt(a.length()-1)).compareTo(String.valueOf(b.charAt(b.length()-1))))
+                .sorted((a, b) -> String.valueOf(a.charAt(a.length() - 1)).compareTo(String.valueOf(b.charAt(b.length() - 1))))
                 .collect(Collectors.joining(", ")));
 
+        Function<PersonForTest, String> personToFullNameFunction = a -> a.getName() + " " + a.getSurname();
+        Comparator<PersonForTest> personForTestComparator = (l, m) -> l.getAge().compareTo(m.getAge());
         System.out.println(people.stream()
-                .sorted((l, m) -> l.getAge().compareTo(m.getAge()))
-                .map(a -> a.getName() + " " + a.getSurname())
+                .sorted(personForTestComparator)
+                .map(personToFullNameFunction)
                 .collect(Collectors.joining(", ")));
+
+        Function<PersonForTest, Integer> function = e -> e.getAge();
 
         Map<Integer, List<PersonForTest>> collect1 = people.stream()
-                .collect(Collectors.groupingBy(e -> e.getAge()));
+                .collect(Collectors.groupingBy(function));
         System.out.println(collect1);
 
         Map<Integer, PersonForTest> collect = people.stream()
                 .collect(Collectors.toMap(e -> e.getIndex(), v -> v));
 
+        Function<PersonForTest, String> getAgeWithEM = a -> a.getAge() + "!";
+        Function<PersonForTest, String> anonymous = new Function<PersonForTest, String>() {
+            @Override
+            public String apply(PersonForTest personForTest) {
+                return personForTest.getAge() + "!";
+            }
+        };
+
 
     }
+
     @Getter
     @ToString
     private class PersonForTest {
@@ -79,4 +93,49 @@ public class StreamsAndLambdasTest {
         }
     }
 
+    @FunctionalInterface
+    public interface SuperChecker<T> {
+        boolean check(T number);
+
+    }
+
+    private SuperChecker<Integer> checkIfOddLambda = a -> a % 2 != 0;
+    private SuperChecker<Integer> checkIfOddAnonymous = new SuperChecker<Integer>() {
+        @Override
+        public boolean check(Integer number) {
+            return number % 2 != 0;
+        }
+    };
+
+    private SuperChecker<Integer> checkIfNumberIsLessThan5 = a -> a < 5;
+    private SuperChecker<Integer> checkIfNumberIsLessThan5Annonymous = new SuperChecker<Integer>() {
+        @Override
+        public boolean check(Integer number) {
+            return number < 5;
+        }
+    };
+
+    @Test
+    public void checker() {
+        System.out.println(checkIfOddLambda.check(1));
+        System.out.println(checkIfOddAnonymous.check(4));
+        System.out.println(checkIfNumberIsLessThan5.check(7));
+        System.out.println(checkIfNumberIsLessThan5Annonymous.check(4));
+
+    }
+
+    @FunctionalInterface
+    public interface MathOperation {
+        Integer calculate(Integer firstInt, Integer secondInt);
+    }
+
+    private MathOperation adder = (a, b) -> a + b;
+    private MathOperation subtracker = (a, b) -> a - b;
+    private MathOperation multiplier = (a, b) -> a * b;
+    @Test
+    public void mathOperationChecker(){
+        System.out.println(adder.calculate(3, 8));
+        System.out.println(subtracker.calculate(3, 8));
+        System.out.println(multiplier.calculate(3, 8));
+    }
 }
