3a1d3dad54dcd0815e397e9aed5bc174303d6b66
piotr <pietia88@gmail.com>
2018-12-28 00:51:33 +0100

    completed methods saveUser and getUserList in class UserDAO

--- src/main/java/pl/sda/intermediate12/App.java
+++ src/main/java/pl/sda/intermediate12/App.java
@@ -1,9 +1,12 @@
 package pl.sda.intermediate12;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class);
--- src/main/java/pl/sda/intermediate12/users/UserDAO.java
+++ src/main/java/pl/sda/intermediate12/users/UserDAO.java
@@ -1,26 +1,75 @@
 package pl.sda.intermediate12.users;
 
 import lombok.Getter;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
-import java.io.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Service
-public class UserDAO {
 
+
+public class UserDAO {
     private static DataSource datasource;
 
-    public List<User> getUserList(){
+    public List<User> getUserList() {
+        List<User> users = new ArrayList<>();
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            String sql = "SELECT * FROM users;";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                User nextUser = new User();
+                int userId = rs.getInt(1);
+                nextUser.setFirstName(rs.getString(2));
+                nextUser.setLastName(rs.getString(3));
+                nextUser.setEMail(rs.getString(4));
+                nextUser.setPasswordHash((rs.getString(5)));
+                nextUser.setBirthDate(rs.getString(6));
+                nextUser.setPesel(rs.getString(7));
+                nextUser.setPhone(rs.getString(8));
+                nextUser.setPreferEmails(rs.getBoolean(9));
+                nextUser.setUserAddress(getUserAddress(userId, connection));
+                users.add(nextUser);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
+        return users;
+    }
+
+    private UserAddress getUserAddress(int userId, Connection connection) {
+        if (connection != null) {
+            try {
+                String sql = "SELECT city, country, zipCode, street FROM userAddresses WHERE userId = ?;";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setInt(1, userId);
+                ResultSet rs = ps.executeQuery();
+                UserAddress userAddress = new UserAddress();
+                if (rs.next()) {
+                    userAddress.setCity(rs.getString(1));
+                    userAddress.setCountry(rs.getString(2));
+                    userAddress.setZipCode(rs.getString(3));
+                    userAddress.setStreet(rs.getString(4));
+                }
+                return userAddress;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
 
@@ -45,14 +94,31 @@ public class UserDAO {
         }
     }
 
+    public void saveUserAddress(UserAddress userAddress, int userId, Connection connection) {
+        if (connection != null) {
+            try {
+                String sqlInsertAddress = "INSERT INTO useraddresses (userId, city, country, zipCode, street) " +
+                        "VALUES (?, ?, ?, ?, ?);";
+                PreparedStatement psa = connection.prepareStatement(sqlInsertAddress);
+                psa.setInt(1, userId);
+                psa.setString(2, userAddress.getCity());
+                psa.setString(3, userAddress.getCountry());
+                psa.setString(4, userAddress.getZipCode());
+                psa.setString(5, userAddress.getStreet());
+                psa.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public void saveUser(User user) {
         Connection connection = null;
         try {
             connection = getConnection();
-            String sql = "INSERT INTO users (firstName, lastName, eMail, passwordHash, birthDate, pesel, phone, preferEmails)" +
+            String sql = "INSERT INTO users (firstName, lastName, eMail, passwordHash, birthDate, pesel, phone, preferEmails) " +
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
-            PreparedStatement ps = connection.prepareStatement(sql);
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
             ps.setString(3, user.getEMail());
@@ -62,6 +128,11 @@ public class UserDAO {
             ps.setString(7, user.getPhone());
             ps.setBoolean(8, user.isPreferEmails());
             ps.executeUpdate();
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                int newUserId = generatedKeys.getInt(1);
+                saveUserAddress(user.getUserAddress(), newUserId, connection);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
